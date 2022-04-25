bfdb3d1816c364eee3dddf845d64d20c11faa98e
Darío Candotti <dario.candotti@gmail.com>
2022-04-25 17:08:07 -0300

    added hello world test

--- src/test/java/hpl/ScriptTest.java
+++ src/test/java/hpl/ScriptTest.java
@@ -1,21 +1,43 @@
 package hpl;
 
 import org.junit.Test;
-import static org.mockito.Mockito.*;
-import java.util.*;
+import static org.junit.Assert.assertEquals;
 
 public class ScriptTest
 {
 	@Test
 	public void shouldDisplayHello ()
 	{
-		Output output = mock(Output.class);
+		FakeOutput output = new FakeOutput();
 
 		new Script("👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊").run(output);
 
-		verify(output).print('H');
-		verify(output).print('e');
-		verify(output, times(2)).print('l');
-		verify(output).print('o');
+		assertEquals("Hello", output.get());
+	}
+
+	@Test
+	public void shouldDisplayHelloWorld ()
+	{
+		FakeOutput output = new FakeOutput();
+
+		new Script("👉👆👆👆👆👆👆👆👆🤜👇👈👆👆👆👆👆👆👆👆👆👉🤛👈👊👉👉👆👉👇🤜👆🤛👆👆👉👆👆👉👆👆👆🤜👉🤜👇👉👆👆👆👈👈👆👆👆👉🤛👈👈🤛👉👇👇👇👇👇👊👉👇👉👆👆👆👊👊👆👆👆👊👉👇👊👈👈👆🤜👉🤜👆👉👆🤛👉👉🤛👈👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👉👊👆👆👆👊👇👇👇👇👇👇👊👇👇👇👇👇👇👇👇👊👉👆👊👉👆👊").run(output);
+
+		// @SEE: why does it print a new line at the end?
+		assertEquals("Hello World!\n", output.get());
+	}
+
+	class FakeOutput
+		implements Output
+	{
+		private StringBuilder output = new StringBuilder();
+
+		@Override
+		public void print (char c) {
+			output.append(c);
+		}
+
+		public String get () {
+			return output.toString();
+		}
 	}
 }
